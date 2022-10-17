6c92e76b15d71ab9c86de1b051cbe512d81d3b96
dffleh <dffleh1995@gmail.com>
2022-10-18 00:14:28 +0300

    I redo everything

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,39 @@
 import { Component } from 'react';
-import FeedbackButtons from './feedback/FeedbackButtons';
-import { FButtons, TitleButtons } from './App.styled';
 
 export class App extends Component {
   state = {
-    buttons: [
-      { id: 'id-1', name: 'good', quantity: 0 },
-      { id: 'id-2', name: 'neutral', quantity: 0 },
-      { id: 'id-3', name: 'bad', quantity: 0 },
-    ],
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
   render() {
     const { buttons } = this.state;
     return (
       <div>
-        <FButtons>
-          <TitleButtons>Please leave feedback</TitleButtons>
-          <FeedbackButtons buttons={buttons} />
-        </FButtons>
+        <div>
+          <h2>Please leave feedback</h2>
+          <ul>
+            <li>
+              <button>Good</button>
+            </li>
+            <li>
+              <button>Neutral</button>
+            </li>
+            <li>
+              <button>Bad</button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <li>Good:</li>
+            <li>Neutral:</li>
+            <li>Bad:</li>
+            <li>Total:</li>
+            <li>Positive feedback:</li>
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+// export const BList = styled.ul `
+//   padding: 15px;
+// `;
+
+// export const BItem = styled.li `
+//   &:not(:last-child) {
+//     margin-bottom: 5px;
+//   }
+// `;
+
+// export const Button = styled.button `
+//   min-width: 100px;
+// `;
